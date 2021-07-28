57c99192a5135ee0ac3b6b215aa59230e8b1fe6a
JI ZHANG <2599081Z@student.gla.ac.uk>
2021-07-28 21:27:09 +0100

    Chapter5 exercise2_3

--- populate_rango.py
+++ populate_rango.py
@@ -34,9 +34,10 @@ def populate():
         {'title':'Flask',
         'url':'http://flask.pocoo.org'} ]
 
-    cats = {'Python': {'pages': python_pages},
-            'Django': {'pages': django_pages},
-            'Other Frameworks': {'pages': other_pages} }
+    cats = {'Python': {'pages': python_pages,'views': 128, 'likes': 64},
+            'Django': {'pages': django_pages,'views': 64, 'likes': 32},
+            'Other Frameworks': {'pages': other_pages,'views': 32, 'likes': 16}
+             }
 
     for cat, cat_data in cats.items():
         c = add_cat(cat)
@@ -54,8 +55,10 @@ def add_page(cat, title, url, views=0):
     p.save()
     return p
 
-def add_cat(name):
+def add_cat(name,likes=0,views=0):
     c = Category.objects.get_or_create(name=name)[0]
+    c.likes = likes
+    c.views = views
     c.save()
     return c
 
